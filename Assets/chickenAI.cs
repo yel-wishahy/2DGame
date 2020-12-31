1cfa561b6550015e9d67b94451a59c8cde55c4f6
yel-wishahy <yel.wishahy@gmail.com>
2020-12-31 04:45:42 -0800

    useless chicken idle animation

--- Assets/chickenAI.cs
+++ Assets/chickenAI.cs
@@ -8,21 +8,19 @@ public class chickenAI : MonoBehaviour
     private Rigidbody2D body;
     private SpriteRenderer image;
     private new Collider2D collider;
-    private float moveCount;
-    private float jumpCount;
-    private float attackCount;
-    private float dir;
+    private float attackTimer, turnTimer, jumpTimer, dir;
 
     //puvblic stuff
     public LayerMask playerLayer;
     public Transform attack;
-    public float speed, chargeSpeed, attackDamage, seekRadius, turnTimer, jumpTimer, jumpSpeed, attackSpeed;
+    public float speed, chargeSpeed, attackDamage, seekRadius, jumpSpeed, attackSpeed;
+    public int timer;
     public chickenState state = chickenState.idle;
     public Vector3 attackRange;
 
     public enum chickenState
     {
-        idle, charge, attack
+       moving, charge, charging, attack, idle
     }
 
     // Start is called before the first frame update
@@ -31,82 +29,96 @@ public class chickenAI : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
         image = GetComponent<SpriteRenderer>();
         collider = GetComponent<Collider2D>();
-        moveCount = 0;
-        jumpCount = 0;
+
+        attackTimer = 0;
+        jumpTimer = 0;
+        turnTimer = 0;
+
         dir = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        Collider2D enemy = null;
-
-        Collider2D[] possibleEnemies = Physics2D.OverlapCircleAll(body.position, seekRadius, playerLayer);
-
-        foreach(Collider2D pe in possibleEnemies)
-        {
-                enemy = pe;
-
-            if (DetectAttackCollision()) 
-            {
-                state = chickenState.attack;
-            }
-            else state = chickenState.charge;
-        }
-
-        switch (state)
-        {
-            case chickenState.idle:
-                if (Random.Range(0.0f, 1.0f) >= 0.25f) Move();
-                else
-                {
-                    Jump();
-                }
-                break;
-            case chickenState.charge:
-                Charge(enemy);
-                state = chickenState.idle;
-                break;
-            case chickenState.attack:
-                Attack(enemy);
-                state = chickenState.idle;
-                break;
-        }
+        if (state != chickenState.charging) changeDir();
+        Move();
+        Jump();
+        Attack();
     }
 
     void Move()
     {
-        if (moveCount > turnTimer)
+        body.velocity = new Vector2(dir * speed, body.velocity.y);
+
+        if (dir == -1) image.flipX = false;
+        else image.flipX = true;
+    }
+
+    void changeDir()
+    {
+        if (turnTimer > timer)
         {
-            moveCount = 0;
+            turnTimer = 0;
             dir *= -1;
         }
 
-        if (dir == 1) image.flipX = true;
-        else image.flipX = false;
-     
-
-        body.velocity = new Vector2(dir * speed, body.velocity.y);
-
-        moveCount++;
+        turnTimer++;
     }
 
-    void Attack(Collider2D enemy)
+    void Jump()
     {
-        if (attackCount > attackSpeed)
+        if (jumpTimer > timer * 3 && body.velocity.y == 0)
         {
-            enemy.GetComponent<Health>().takeDamage(attackDamage);
-            attackCount = 0;
+            jumpTimer = 0;
+            body.velocity = new Vector2(body.velocity.x, jumpSpeed);
         }
 
-        attackCount++;
-       
+        jumpTimer++;
+
+    }
+
+    void Attack()
+    {
+        Collider2D enemy = null;
+
+        Collider2D[] possibleEnemies = Physics2D.OverlapCircleAll(body.position, seekRadius, playerLayer);
+
+        if (possibleEnemies.Length < 1) state = chickenState.moving;
+
+        foreach (Collider2D pe in possibleEnemies)
+        {
+            enemy = pe;
+
+            if (DetectAttackCollision())
+            {
+                state = chickenState.attack;
+            }
+            else if (state != chickenState.charge || state != chickenState.charging)
+            {
+                state = chickenState.charge;
+            }
+        }
+
+
+        if (state == chickenState.attack)
+        {
+            if (attackTimer > attackSpeed)
+            {
+                enemy.GetComponent<Health>().takeDamage(attackDamage);
+                attackTimer = 0;
+            }
+
+            attackTimer++;
+            state = chickenState.moving;
+        } else if (state == chickenState.charge || state == chickenState.charging)
+        {
+            Charge(enemy);
+            state = chickenState.charging;
+        }
     }
 
     private bool DetectAttackCollision()
     {
-
         Collider2D attackCollide = Physics2D.OverlapBox(attack.position, attackRange, 0, playerLayer);
 
         if (attackCollide != null)
@@ -119,19 +131,6 @@ public class chickenAI : MonoBehaviour
 
     }
 
-
-    void Jump()
-    {
-        if (jumpCount > jumpTimer)
-        {
-            jumpCount = 0;
-            body.velocity = new Vector2(body.velocity.x, jumpSpeed);
-        }
-        
-        jumpCount++;
-
-    }
-
     void Charge(Collider2D enemy)
     {
         float dir = 1;
@@ -140,10 +139,10 @@ public class chickenAI : MonoBehaviour
             dir *= -1;
         }
 
-        if (dir == 1) image.flipX = true;
-        else image.flipX = false;
-
         body.velocity = new Vector2(dir * chargeSpeed, body.velocity.y);
+
+        if (dir == -1) image.flipX = false;
+        else image.flipX = true;
     }
 
     //for testing
