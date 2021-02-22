6faedfe59a2126ddfa8cc935a76e8e5ae41fa20f
yel-wishahy <yel.wishahy@gmail.com>
2021-02-21 18:17:13 -0800

    added kfc item, some more directory organization

--- Assets/Internal Assets/Items/Coal/Coal.cs
+++ Assets/Internal Assets/Items/Coal/Coal.cs
@@ -8,10 +8,8 @@ public class Coal : Item
 {
     private void  OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("above");
         if (collision.GetComponent<Player>() != null)
         {
-            Debug.Log("above2");
             Player collector = collision.GetComponent<Player>();
             collector.pickupItem(this);
             Destroy(this);
---
+++ Assets/Internal Assets/Items/KFC/KFC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KFC : Item
+{
+    public float healthBuff;
+    
+    private void  OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() != null)
+        {
+            Player collector = collision.GetComponent<Player>();
+            collector.pickupItem(this);
+            Destroy(this);
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+        
+    }
+    
+}
