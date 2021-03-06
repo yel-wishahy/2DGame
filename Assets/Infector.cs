fbf6443b966167ebba06d5ddd7a9edcca820a4d5
kenny zhang <ktzhang@sfu.ca>
2021-03-05 21:51:32 -0800

    Added infection mechanic

---
+++ Assets/Infector.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infector : MonoBehaviour
+{
+    public float InfectInterval = 2.5f; // Time before damage from debuff
+    public int MinimumHealth = 25;
+    public float InfectDmg = 5f;
+
+    public string[] InfectTag = { "NPC", "Player", "MainCharacter" };
+
+    public LayerMask DefaultLayer;
+    public float SeekRadius = 2.0f;
+
+    UEntity currEntity;
+    bool damage = false;
+    float currentTime = 0.0f;
+
+    Rigidbody2D body;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currEntity = GetComponent<UEntity>();
+
+        foreach (string comp in InfectTag)
+        {
+            if (this.gameObject.tag == comp)
+            {
+                damage = true;
+                break;
+            }
+        }
+
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (damage)
+        {
+            if (currentTime < InfectInterval)
+            {
+                currentTime += Time.deltaTime;
+            }
+            else
+            {
+                if (currEntity.Health > MinimumHealth)
+                    currEntity.setHealth(currEntity.Health - InfectDmg);
+
+                currentTime = 0;
+            }
+        }
+        else
+        {
+            Collider2D[] possibleEnemies = Physics2D.OverlapCircleAll(body.position, SeekRadius, DefaultLayer);
+
+            print(possibleEnemies);
+
+            foreach (Collider2D entity in possibleEnemies)
+            {
+                if (entity.gameObject.GetComponent<Infector>() == null)
+                    entity.gameObject.AddComponent<Infector>();
+            }
+        }
+    }
+}
