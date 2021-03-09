9023f6d1942ed66ca26b0e77adde5e45f33d2fd8
yel-wishahy <yel.wishahy@gmail.com>
2021-03-09 15:26:17 -0800

    Added some new assets, text mesh pro GUI is now utilized instead of text for the dialogue box since it has a paging feature as well as better visual cotnrol (we can add nice effects later on!), also updated unity

--- Assets/Internal Assets/Dialogue/DialogueUI.cs
+++ Assets/Internal Assets/Dialogue/DialogueUI.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogueUI : MonoBehaviour
 {
     [SerializeField] private TextAsset jsonDialogue;
-    [SerializeField] private Text textPanel;
     [SerializeField] private Text characterNamePanel;
+    [SerializeField] private TextMeshProUGUI textPanel;
     [SerializeField] private float textScrollSpeed;
 
     private Dictionary<string, string> currentLine;
@@ -22,11 +23,14 @@ public class DialogueUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        textPanel.pageToDisplay = textPanel.textInfo.pageCount;
+        
         if (Input.GetKeyDown(KeyCode.E))
         {
             currentLine = dialogue.NextLine();
             characterNamePanel.text = currentLine["name"];
-            
+
+            textPanel.pageToDisplay = 1;
             StopAllCoroutines();
             StartCoroutine(DisplayText(currentLine["text"]));
 
